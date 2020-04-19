5a47bdf95d95d1fc7e36f2f3697b85445cb6eb14
Kennedy Sigauke <carnag3kid7@gmail.com>
2020-04-19 13:45:21 +0200

    Character card test fixes

--- src/app/character/components/character-card/character-card.component.spec.ts
+++ src/app/character/components/character-card/character-card.component.spec.ts
@@ -3,11 +3,13 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CharacterCardComponent } from './character-card.component';
 import {Character} from '../../../models/character';
 import * as moment from 'moment';
+import {By} from '@angular/platform-browser';
 import {RouterTestingModule} from '@angular/router/testing';
 
-describe('CharacterComponent', () => {
+describe('CharacterCardComponent', () => {
   let component: CharacterCardComponent;
   let fixture: ComponentFixture<CharacterCardComponent>;
+  let CHARACTER;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -17,24 +19,36 @@ describe('CharacterComponent', () => {
       ]
     })
     .compileComponents();
-  }));
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(CharacterCardComponent);
     component = fixture.componentInstance;
-
-    component.character = {
+    CHARACTER = {
       id: 1,
       name: 'Rick Sanchez',
+      image: '',
+      episode: ['1', '2', '3', '4', '5'],
       created: moment().subtract(2, 'days').toString()
     } as Character;
 
+  }));
+
+  it('createdDate returns humanized date', () => {
+    component.character = CHARACTER;
     fixture.detectChanges();
+
+    expect(component.createdDate).toBe('2 days ago');
   });
 
-  describe('Character card', () => {
-    it('#createdDate returns humanized date', () => {
-       expect(component.createdDate).toBe('2 days ago');
-    });
+  it('should navigate to view episode', () => {
+    component.character = CHARACTER;
+    fixture.detectChanges();
+
+    const viewEpisodesFun = spyOn(component, 'viewEpisodes');
+
+    const viewEpisodeBtn = fixture.debugElement.query(By.css('button'));
+
+    viewEpisodeBtn.triggerEventHandler('click', null);
+
+    expect(viewEpisodesFun).toHaveBeenCalledWith(CHARACTER);
   });
 });
